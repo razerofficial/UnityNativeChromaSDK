7267add76a3f6cda8160a069a47c19bfec11485a
Tim Graupmann <tgraupmann@gmail.com>
2017-08-04 10:45:27 -0700

    Added DLL interface for Chroma

--- Assets/UnityNativeChromaSDK/Scripts/UnityNativeChromaSDK.cs
+++ Assets/UnityNativeChromaSDK/Scripts/UnityNativeChromaSDK.cs
@@ -1,15 +1,132 @@
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
 using UnityEngine;
-using System.Collections;
 
-public class UnityNativeChromaSDK : MonoBehaviour {
+namespace ChromaSDK
+{
+    public class UnityNativeChromaSDK
+    {
+#if UNITY_3 || UNITY_3_0 || UNITY_3_1 || UNITY_3_2 || UNITY_3_3 || UNITY_3_4 || UNITY_3_5
+        const string DLL_NAME = "UnityNativeChromaSDK3";
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+#else
+    const string DLL_NAME = "UnityNativeChromaSDK";
+#endif
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+        /// <summary>
+        /// Is the editor dialog open?
+        /// </summary>
+        /// <returns></returns>
+        [DllImport(DLL_NAME)]
+        public static extern bool PluginIsDialogOpen();
+
+        /// <summary>
+        /// Open the editor dialog
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        [DllImport(DLL_NAME)]
+        private static extern int PluginOpenEditorDialog(IntPtr path);
+
+        /// <summary>
+        /// Open a chroma animation, returns -1 if failed to open, otherwise returns the id of the animation
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        [DllImport(DLL_NAME)]
+        private static extern int PluginOpenAnimation(IntPtr path);
+
+        /// <summary>
+        /// Load animation effects
+        /// </summary>
+        /// <param name="animationId"></param>
+        /// <returns></returns>
+        [DllImport(DLL_NAME)]
+        public static extern int PluginLoadAnimation(int animationId);
+
+        /// <summary>
+        /// Unload animation effects
+        /// </summary>
+        /// <param name="animationId"></param>
+        /// <returns></returns>
+        [DllImport(DLL_NAME)]
+        public static extern int PluginUnloadAnimation(int animationId);
+
+        /// <summary>
+        /// Play animation
+        /// </summary>
+        /// <param name="animationId"></param>
+        /// <returns></returns>
+        [DllImport(DLL_NAME)]
+        public static extern int PluginPlayAnimation(int animationId);
+
+        /// <summary>
+        /// Stop animation
+        /// </summary>
+        /// <param name="animationId"></param>
+        /// <returns></returns>
+        [DllImport(DLL_NAME)]
+        public static extern int PluginStopAnimation(int animationId);
+
+        /// <summary>
+        /// Close animation
+        /// </summary>
+        /// <param name="animationId"></param>
+        /// <returns></returns>
+        [DllImport(DLL_NAME)]
+        public static extern int PluginCloseAnimation(int animationId);
+
+        /// <summary>
+        /// Open an animation file
+        /// Returns -1 if animation not found
+        /// Returns >= 0 if animation has opened
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static int OpenAnimation(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                return - 1;
+            }
+            FileInfo fi = new FileInfo(path);
+            if (fi.Exists)
+            {
+                byte[] array = ASCIIEncoding.ASCII.GetBytes(fi.FullName);
+                IntPtr lpData = Marshal.AllocHGlobal(array.Length);
+                Marshal.Copy(array, 0, lpData, array.Length);
+                int animationId = PluginOpenAnimation(lpData);
+                Marshal.FreeHGlobal(lpData);
+                return animationId;
+            }
+            return -1;
+        }
+
+        public static int EditAnimation(string path)
+        {
+            if (PluginIsDialogOpen())
+            {
+                return -1;
+            }
+            if (string.IsNullOrEmpty(path))
+            {
+                return -1;
+            }
+            FileInfo fi = new FileInfo(path);
+            if (fi.Exists)
+            {
+                byte[] array = ASCIIEncoding.ASCII.GetBytes(fi.FullName);
+                IntPtr lpData = Marshal.AllocHGlobal(array.Length);
+                Marshal.Copy(array, 0, lpData, array.Length);
+                int animationId = PluginOpenEditorDialog(lpData);
+                Marshal.FreeHGlobal(lpData);
+                return animationId;
+            }
+            return -1;
+        }
+    }
 }
+
+#endif
--- Assets/UnityNativeChromaSDK/Scripts/UnityNativeChromaSDKExample01.cs
+++ Assets/UnityNativeChromaSDK/Scripts/UnityNativeChromaSDKExample01.cs
@@ -1,3 +1,5 @@
+using ChromaSDK;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UnityNativeChromaSDKExample01 : MonoBehaviour
@@ -20,8 +22,34 @@ public class UnityNativeChromaSDKExample01 : MonoBehaviour
         "RandomMouseEffect.chroma",
         "RandomMousepadEffect.chroma",
     };
+    Dictionary<string, int> _mLoadedAnimations = new Dictionary<string, int>();
+    string GetPath(string animation)
+    {
+        return string.Format("{0}/{1}", Application.streamingAssetsPath, animation);
+    }
+    int GetAnimation(string animation)
+    {
+        string path = GetPath(animation);
+        int animationid;
+        if (!_mLoadedAnimations.ContainsKey(animation))
+        {
+            animationid = UnityNativeChromaSDK.OpenAnimation(path);
+            _mLoadedAnimations[animation] = animationid;
+        }
+        else
+        {
+            animationid = _mLoadedAnimations[animation];
+        }
+        return animationid;
+    }
+    int EditAnimation(string animation)
+    {
+        string path = GetPath(animation);
+        return UnityNativeChromaSDK.EditAnimation(path);
+    }
     private void OnGUI()
     {
+        bool dialogIsOpen = UnityNativeChromaSDK.PluginIsDialogOpen();
         GUILayout.BeginVertical(GUILayout.Height(Screen.height));
         GUILayout.FlexibleSpace();
         foreach (string animation in _mAnimations)
@@ -31,12 +59,26 @@ public class UnityNativeChromaSDKExample01 : MonoBehaviour
             GUILayout.Label(animation);
             if (GUILayout.Button("Play", GUILayout.Height(30)))
             {
-
+                int animationId = GetAnimation(animation);
+                if (animationId >= 0)
+                {
+                    UnityNativeChromaSDK.PluginPlayAnimation(animationId);
+                }                
             }
             if (GUILayout.Button("Stop", GUILayout.Height(30)))
             {
-
+                int animationId = GetAnimation(animation);
+                if (animationId >= 0)
+                {
+                    UnityNativeChromaSDK.PluginStopAnimation(animationId);
+                }
             }
+            GUI.enabled = !dialogIsOpen;
+            if (GUILayout.Button("Edit", GUILayout.Height(30)))
+            {
+                EditAnimation(animation);
+            }
+            GUI.enabled = true;
             GUILayout.FlexibleSpace();
             GUILayout.EndHorizontal();
         }
