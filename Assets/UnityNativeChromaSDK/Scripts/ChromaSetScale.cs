e894d8bf94a15cf60098b27788b5651fa196f2d0
Tim Graupmann <tgraupmann@gmail.com>
2018-05-15 11:32:19 -0700

    Decoupled set scale

---
+++ Assets/UnityNativeChromaSDK/Scripts/ChromaSetScale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class ChromaSetScale : MonoBehaviour
+{
+    public Vector3 _mScale = Vector3.one;
+
+    private void Update()
+    {
+        transform.localScale = _mScale;
+    }
+}
