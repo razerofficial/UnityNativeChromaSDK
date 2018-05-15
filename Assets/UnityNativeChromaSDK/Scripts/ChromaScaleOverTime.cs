a2acdcea48317c31e514b74b28f483c493f67830
Tim Graupmann <tgraupmann@gmail.com>
2018-05-15 10:35:21 -0700

    Added script to scale over time in the editor

---
+++ Assets/UnityNativeChromaSDK/Scripts/ChromaScaleOverTime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class ChromaScaleOverTime : MonoBehaviour
+{
+    public bool _mIsEnabled = false;
+    public Vector3 _mScale = Vector3.one;
+    public float _mSpeed = 1f;
+    public Vector3 _mStartScale = Vector3.one;
+    private DateTime _mTimer = DateTime.Now;
+
+    private void Update()
+    {
+        float elapsed = (float)(DateTime.Now - _mTimer).TotalSeconds;
+        _mTimer = DateTime.Now;
+        if (_mIsEnabled)
+        {
+            Vector3 scale = transform.localScale;
+            scale += _mScale * (elapsed * _mSpeed);
+            scale.x = Mathf.Max(scale.x, 0f);
+            scale.y = Mathf.Max(scale.y, 0f);
+            scale.z = Mathf.Max(scale.z, 0f);
+            transform.localScale = scale;
+        }
+        else
+        {
+            transform.localScale = _mStartScale;
+        }
+    }
+}
