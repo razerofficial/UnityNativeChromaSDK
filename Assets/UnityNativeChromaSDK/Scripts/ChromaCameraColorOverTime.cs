ac2217c733af18ae1dbf5a8b9a6811b964dc6a19
Tim Graupmann <tgraupmann@gmail.com>
2018-05-16 12:06:28 -0700

    Added helper script to change camera color over time

---
+++ Assets/UnityNativeChromaSDK/Scripts/ChromaCameraColorOverTime.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class ChromaCameraColorOverTime : MonoBehaviour
+{
+    public Color _mFromColor = Color.black;
+    public Color _mToColor = Color.white;
+    public float _mSpeed = 1f;
+    private DateTime _mTimer = DateTime.Now;
+
+    private void OnEnable()
+    {
+        _mTimer = DateTime.Now;
+    }
+
+    private void Update()
+    {
+        float elapsed = (float)(DateTime.Now - _mTimer).TotalSeconds;
+        Color color = Color.Lerp(_mFromColor, _mToColor, elapsed * _mSpeed);
+
+        Camera camera = GetComponent<Camera>();
+        if (camera)
+        {
+            camera.backgroundColor = color;
+        }
+    }
+}
---
+++ Assets/UnityNativeChromaSDK/Scripts/ChromaSetCameraColor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class ChromaSetCameraColor : MonoBehaviour
+{
+    public Color _mColor = Color.black;
+
+    private void Update()
+    {
+        Camera camera = GetComponent<Camera>();
+        if (camera)
+        {
+            camera.backgroundColor = _mColor;
+        }
+    }
+}
