300931bea8fc2c91262d48f1765b2f8b37047f07
Tim Graupmann <tgraupmann@gmail.com>
2018-05-24 10:53:49 -0700

    Added helper script

---
+++ Assets/UnityNativeChromaSDK/Scripts/ChromaSetPosition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class ChromaSetPosition : MonoBehaviour
+{
+    public Vector3 _mPosition = Vector3.zero;
+
+    private void OnEnable()
+    {
+        transform.localPosition = _mPosition;
+    }
+}
--- Assets/UnityNativeChromaSDK/Scripts/ChromaTranslateOverTime.cs
+++ Assets/UnityNativeChromaSDK/Scripts/ChromaTranslateOverTime.cs
@@ -6,25 +6,21 @@ using UnityEngine;
 #endif
 public class ChromaTranslateOverTime : MonoBehaviour
 {
-    public bool _mIsEnabled = false;
     public Vector3 _mDirection = Vector3.zero;
     public float _mSpeed = 1f;
-    private Vector3 _mStartPosition = Vector3.zero;
     private DateTime _mTimer = DateTime.Now;
+
+    private void OnEnable()
+    {
+        _mTimer = DateTime.Now;
+    }
 
     private void Update()
     {
         float elapsed = (float)(DateTime.Now - _mTimer).TotalSeconds;
         _mTimer = DateTime.Now;
-        if (_mIsEnabled)
-        {
-            Vector3 pos = transform.position;
-            pos += _mDirection * (elapsed * _mSpeed);
-            transform.position = pos;
-        }
-        else
-        {
-            transform.position = _mStartPosition;
-        }
+        Vector3 pos = transform.position;
+        pos += _mDirection * (elapsed * _mSpeed);
+        transform.position = pos;
     }
 }
