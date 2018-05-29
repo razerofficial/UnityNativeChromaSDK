e2e844472a04bac1784063f349b8e107ec7b1a13
Tim Graupmann <tgraupmann@gmail.com>
2018-05-29 15:31:53 -0700

    Added helper script to lerp to a color

---
+++ Assets/UnityNativeChromaSDK/Scripts/ChromaMaterialColorOverTime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class ChromaMaterialColorOverTime : MonoBehaviour
+{
+    public Color _mToColor = Color.white;
+    public float _mSpeed = 1f;
+    private DateTime _mTimer = DateTime.Now;
+
+    private void OnEnable()
+    {
+        _mTimer = DateTime.Now;
+    }
+
+    private void Update()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        if (meshRenderer)
+        {
+            Material material = meshRenderer.material;
+            if (material)
+            {
+                Color color = material.color;
+                float elapsed = (float)(DateTime.Now - _mTimer).TotalSeconds;
+                _mTimer = DateTime.Now;
+                color = Color.Lerp(color, _mToColor, elapsed * _mSpeed);
+                material.color = color;
+            }
+        }
+    }
+}
