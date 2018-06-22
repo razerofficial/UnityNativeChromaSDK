3723e87e0e2d9805ef9ac4668484b1e8c83d801f
Tim Graupmann <tgraupmann@gmail.com>
2018-06-21 18:46:06 -0700

    A script for rotation

---
+++ Assets/UnityNativeChromaSDK/Scripts/ChromaRotateOverTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class ChromaRotateOverTime : MonoBehaviour
+{
+    public Vector3 _mRotation = Vector3.zero;
+    public float _mSpeed = 1f;
+    private DateTime _mTimer = DateTime.Now;
+
+    private void OnEnable()
+    {
+        _mTimer = DateTime.Now;
+    }
+
+    private void Update()
+    {
+        float elapsed = (float)(DateTime.Now - _mTimer).TotalSeconds;
+        _mTimer = DateTime.Now;
+        transform.Rotate(_mRotation * _mSpeed * elapsed);
+    }
+}
