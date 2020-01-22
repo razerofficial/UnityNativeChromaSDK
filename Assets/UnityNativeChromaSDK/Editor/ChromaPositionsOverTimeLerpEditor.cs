6e11748dacf5925868fd3ec9219fee2591ec04d3
Timothy Graupmann <tim.graupmann@razer.com>
2020-01-22 08:30:01 -0800

    Added custom editor for adding points

---
+++ Assets/UnityNativeChromaSDK/Editor/ChromaPositionsOverTimeLerpEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ChromaPositionsOverTimeLerp))]
+public class ChromaPositionsOverTimeLerpEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Add Point"))
+        {
+            if (target)
+            {
+                ChromaPositionsOverTimeLerp item = (ChromaPositionsOverTimeLerp)target;
+                if (item._mPositions != null)
+                {
+                    Vector3[] positions = new Vector3[item._mPositions.Length + 1];
+                    System.Array.Copy(item._mPositions, positions, item._mPositions.Length);
+                    Vector3 pos = item.transform.position;
+                    positions[positions.Length - 1] = pos;
+                    item._mPositions = positions;
+                }
+            }
+        }
+        base.OnInspectorGUI();
+    }
+}
