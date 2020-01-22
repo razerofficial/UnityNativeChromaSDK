627f27e8aaf8050e60d2fade968753e267a7ebcf
Timothy Graupmann <tim.graupmann@razer.com>
2020-01-22 08:21:54 -0800

    Added script to lerp between array of positions

---
+++ Assets/UnityNativeChromaSDK/Scripts/ChromaPositionsOverTimeLerp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class ChromaPositionsOverTimeLerp : MonoBehaviour
+{
+    public Vector3[] _mPositions = new Vector3[1];
+    public float _mSpeed = 1f;
+    private DateTime _mTimer = DateTime.Now;
+	private float _mTime = 0;
+
+    private void OnEnable()
+    {
+        _mTimer = DateTime.Now;
+		_mTime = 0;
+    }
+
+    private void Update()
+    {
+        float elapsed = (float)(DateTime.Now - _mTimer).TotalSeconds;
+        _mTimer = DateTime.Now;
+		_mTime += _mSpeed * elapsed;
+        float t = 1 - Mathf.Abs(Mathf.Cos(Mathf.PI * _mTime));
+		Vector3 pos = transform.position;
+        if (_mPositions.Length > 2)
+        {
+            pos = NG.Vector3Ext.MultiLerp(_mPositions, t);
+        }
+        else if (_mPositions.Length == 2)
+        {
+            Vector3 pos1 = _mPositions[0];
+            Vector3 pos2 = _mPositions[1];
+            pos.x = Mathf.Lerp(pos1.x, pos2.x, t);
+            pos.y = Mathf.Lerp(pos1.y, pos2.y, t);
+            pos.z = Mathf.Lerp(pos1.z, pos2.z, t);
+        }
+        else if (_mPositions.Length == 1)
+        {
+            pos.x = _mPositions[0].x;
+            pos.y = _mPositions[0].y;
+            pos.z = _mPositions[0].z;
+        }
+        transform.localPosition = pos;
+    }
+}
---
+++ Assets/UnityNativeChromaSDK/Scripts/Vector3Ext.cs
@@ -0,0 +1,83 @@
+﻿// Ref: https://gist.github.com/Naphier/46e066746ee9eeb70ebfd5062dc426bf
+
+using UnityEngine;
+
+namespace NG
+{
+    public static class Vector3Ext
+    {
+        #region Multilerp
+        // This is not very optimized. There are at least n + 1 and at most 2n Vector3.Distance
+        // calls (where n is the number of waypoints). 
+        public static Vector3 MultiLerp(Vector3[] waypoints, float ratio)
+        {
+            Vector3 position = Vector3.zero;
+            float totalDistance = waypoints.MultiDistance();
+            float distanceTravelled = totalDistance * ratio;
+
+            int indexLow = GetVectorIndexFromDistanceTravelled(waypoints, distanceTravelled);
+            int indexHigh = indexLow + 1;
+
+            // we're done
+            if (indexHigh > waypoints.Length - 1)
+                return waypoints[waypoints.Length - 1];
+
+
+            // calculate the distance along this waypoint to the next
+            Vector3[] completedWaypoints = new Vector3[indexLow + 1];
+
+            for (int i = 0; i < indexLow + 1; i++)
+            {
+                completedWaypoints[i] = waypoints[i];
+            }
+
+            float distanceCoveredByPreviousWaypoints = completedWaypoints.MultiDistance();
+            float distanceTravelledThisSegment = distanceTravelled - distanceCoveredByPreviousWaypoints;
+            float distanceThisSegment = Vector3.Distance(waypoints[indexLow], waypoints[indexHigh]);
+
+            float currentRatio = distanceTravelledThisSegment / distanceThisSegment;
+            position = Vector3.Lerp(waypoints[indexLow], waypoints[indexHigh], currentRatio);
+
+            return position;
+        }
+
+        public static float MultiDistance(this Vector3[] waypoints)
+        {
+            float distance = 0f;
+
+            for (int i = 0; i < waypoints.Length; i++)
+            {
+                if (i + 1 > waypoints.Length - 1)
+                    break;
+
+                distance += Vector3.Distance(waypoints[i], waypoints[i + 1]);
+            }
+
+            return distance;
+        }
+
+        public static int GetVectorIndexFromDistanceTravelled(Vector3[] waypoints, float distanceTravelled)
+        {
+            float distance = 0f;
+
+            for (int i = 0; i < waypoints.Length; i++)
+            {
+                if (i + 1 > waypoints.Length - 1)
+                    return waypoints.Length - 1;
+
+                float segmentDistance = Vector3.Distance(waypoints[i], waypoints[i + 1]);
+
+                if (segmentDistance + distance > distanceTravelled)
+                {
+                    return i;
+                }
+
+                distance += segmentDistance;
+            }
+
+            return waypoints.Length - 1;
+        }
+        #endregion
+
+    }
+}
