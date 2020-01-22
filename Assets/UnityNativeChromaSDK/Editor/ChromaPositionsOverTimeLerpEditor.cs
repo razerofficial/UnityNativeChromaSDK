8b449463b49443dc8e9b6f871cbe29f16ad163bc
Timothy Graupmann <tim.graupmann@razer.com>
2020-01-22 14:11:57 -0800

    Key 'P' adds a point

--- Assets/UnityNativeChromaSDK/Editor/ChromaPositionsOverTimeLerpEditor.cs
+++ Assets/UnityNativeChromaSDK/Editor/ChromaPositionsOverTimeLerpEditor.cs
@@ -4,22 +4,47 @@ using UnityEditor;
 [CustomEditor(typeof(ChromaPositionsOverTimeLerp))]
 public class ChromaPositionsOverTimeLerpEditor : Editor
 {
+    void AddPoint()
+    {
+        if (target)
+        {
+            ChromaPositionsOverTimeLerp item = (ChromaPositionsOverTimeLerp)target;
+            if (item._mPositions != null)
+            {
+                Vector3[] positions = new Vector3[item._mPositions.Length + 1];
+                System.Array.Copy(item._mPositions, positions, item._mPositions.Length);
+                Vector3 pos = item.transform.position;
+                positions[positions.Length - 1] = pos;
+                item._mPositions = positions;
+            }
+        }
+    }
+
+    void OnSceneGUI()
+    {
+        Event e = Event.current;
+        switch (e.type)
+        {
+            case EventType.keyUp:
+                {
+                    if (Event.current.keyCode == (KeyCode.P))
+                    {
+                        AddPoint();
+                    }
+                    break;
+                }
+        }
+    }
+
     public override void OnInspectorGUI()
     {
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            Debug.Log("Add Point");
+        }
         if (GUILayout.Button("Add Point"))
         {
-            if (target)
-            {
-                ChromaPositionsOverTimeLerp item = (ChromaPositionsOverTimeLerp)target;
-                if (item._mPositions != null)
-                {
-                    Vector3[] positions = new Vector3[item._mPositions.Length + 1];
-                    System.Array.Copy(item._mPositions, positions, item._mPositions.Length);
-                    Vector3 pos = item.transform.position;
-                    positions[positions.Length - 1] = pos;
-                    item._mPositions = positions;
-                }
-            }
+            AddPoint();
         }
         base.OnInspectorGUI();
     }
