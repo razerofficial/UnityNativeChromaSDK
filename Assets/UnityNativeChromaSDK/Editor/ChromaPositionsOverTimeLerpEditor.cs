0a74ecf4c94f1c51b8b8f1a439eaf6fe0ee20727
Timothy Graupmann <tim.graupmann@razer.com>
2020-01-22 17:12:57 -0800

    Added keyboard shortcuts

--- Assets/UnityNativeChromaSDK/Editor/ChromaPositionsOverTimeLerpEditor.cs
+++ Assets/UnityNativeChromaSDK/Editor/ChromaPositionsOverTimeLerpEditor.cs
@@ -4,6 +4,7 @@ using UnityEditor;
 [CustomEditor(typeof(ChromaPositionsOverTimeLerp))]
 public class ChromaPositionsOverTimeLerpEditor : Editor
 {
+    private int _mIndex = 0;
     void AddPoint()
     {
         if (target)
@@ -20,35 +21,130 @@ public class ChromaPositionsOverTimeLerpEditor : Editor
         }
     }
 
+    void UpdatePoint()
+    {
+        if (target)
+        {
+            ChromaPositionsOverTimeLerp item = (ChromaPositionsOverTimeLerp)target;
+            if (item._mPositions != null &&
+                item._mPositions.Length > 0 &&
+                _mIndex < item._mPositions.Length)
+            {
+                item._mPositions[_mIndex] = item.transform.position;
+            }
+        }
+    }
+
+    void MoveToPoint()
+    {
+        if (target)
+        {
+            ChromaPositionsOverTimeLerp item = (ChromaPositionsOverTimeLerp)target;
+            if (item._mPositions.Length > 0 &&
+                _mIndex < item._mPositions.Length)
+            {
+                item.transform.position = item._mPositions[_mIndex];
+            }
+        }
+    }
+
+    void PreviousPoint()
+    {
+        if (target)
+        {
+            ChromaPositionsOverTimeLerp item = (ChromaPositionsOverTimeLerp)target;
+            if (item._mPositions == null)
+            {
+                return;
+            }
+            if (_mIndex > 0)
+            {
+                --_mIndex;
+            }
+            MoveToPoint();
+        }
+    }
+
+    void NextPoint()
+    {
+        if (target)
+        {
+            ChromaPositionsOverTimeLerp item = (ChromaPositionsOverTimeLerp)target;
+            if (item._mPositions == null)
+            {
+                return;
+            }
+            if ((_mIndex + 1) < (item._mPositions.Length - 1))
+            {
+                ++_mIndex;
+            }
+            else
+            {
+                _mIndex = item._mPositions.Length - 1;
+            }
+            MoveToPoint();
+        }
+    }
+
+
     void OnSceneGUI()
     {
         Event e = Event.current;
         switch (e.type)
         {
             case EventType.keyUp:
+                if (Event.current.keyCode == KeyCode.P)
                 {
-                    if (Event.current.keyCode == (KeyCode.P))
-                    {
-                        AddPoint();
-                    }
-                    break;
+                    AddPoint();
                 }
+                else if (Event.current.keyCode == KeyCode.U)
+                {
+                    UpdatePoint();
+                }
+                else if (Event.current.keyCode == KeyCode.LeftBracket)
+                {
+                    PreviousPoint();
+                }
+                else if (Event.current.keyCode == KeyCode.RightBracket)
+                {
+                    NextPoint();
+                }
+                break;
         }
     }
 
     public override void OnInspectorGUI()
     {
-        if (GUILayout.Button("Clear Points"))
+        if (target)
         {
-            if (target)
+            ChromaPositionsOverTimeLerp item = (ChromaPositionsOverTimeLerp)target;
+            if (item._mPositions == null)
             {
-                ChromaPositionsOverTimeLerp item = (ChromaPositionsOverTimeLerp)target;
                 item._mPositions = new Vector3[0];
             }
-        }
-        if (GUILayout.Button("Add Point"))
-        {
-            AddPoint();
+            if (GUILayout.Button("Clear Points"))
+            {
+                item._mPositions = new Vector3[0];
+            }
+            if (GUILayout.Button("(P) Add Point"))
+            {
+                AddPoint();
+            }
+            if (GUILayout.Button("(U) Update Point"))
+            {
+                UpdatePoint();
+            }
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("[ Previous"))
+            {
+                PreviousPoint();
+            }
+            if (GUILayout.Button("] Next"))
+            {
+                NextPoint();
+            }
+            GUILayout.Label(string.Format("Index: {0} of {1}", _mIndex, item._mPositions.Length - 1));
+            GUILayout.EndHorizontal();
         }
         base.OnInspectorGUI();
     }
